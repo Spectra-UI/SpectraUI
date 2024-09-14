f6ed4cf1a091d3b605a95cf5385edb687bbaa746
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-09-14 16:52:24 +0200

    🧽 UPDATE: add portraits texture via code to mmt

---
+++ SpectraUI/misc/portraits.lua
@@ -0,0 +1,26 @@
+mMT.Media.CustomPortraits["spectraui"] = {
+	name = "Spectra UI", -- name to show
+
+	-- portrait texture part
+	texture = "Interface\\AddOns\\SpectraUI\\media\\portrait\\texture.tga",
+	border = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border.tga",
+	shadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\shadow.tga",
+	inner = "Interface\\AddOns\\SpectraUI\\media\\portrait\\inner_shadow.tga",
+
+	-- else use this
+	extraMask = false,
+	mask = "Interface\\AddOns\\SpectraUI\\media\\portrait\\mask.tga",
+
+	-- extra textures for rare, elite and boss
+	rare = "Interface\\AddOns\\SpectraUI\\media\\portrait\\rare_texture.tga",
+	rareborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\rare_border.tga",
+	rareshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\rare_shadow.tga",
+
+	elite = "Interface\\AddOns\\SpectraUI\\media\\portrait\\elite_texture.tga",
+	eliteborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\elite_border.tga",
+	eliteshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\elite_shadow.tga",
+
+	boss = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border.tga",
+	bossborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border_border.tga",
+	bossshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border_shadow.tga",
+}
