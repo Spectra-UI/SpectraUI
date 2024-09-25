c3813db5f09c3f160f178c588668c3001dded3c4
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-09-25 18:31:14 +0200

    🧽 UPDATE: add click function and tooltip to details datatext

--- SpectraUI/datatext/details.lua
+++ SpectraUI/datatext/details.lua
@@ -4,23 +4,42 @@ local DT = E:GetModule("DataTexts")
 --WoW API / Variables
 local _G = _G
 local IsAddOnLoaded = _G.C_AddOns and _G.C_AddOns.IsAddOnLoaded or _G.IsAddOnLoaded
+local Details = _G.Details
 
 local function OnEnter(self)
 	DT.tooltip:ClearLines()
 	DT.tooltip:AddDoubleLine(SpectraUI.Name, SpectraUI.Icon)
-	DT.tooltip:AddDoubleLine("Version:", SpectraUI.Version)
+	DT.tooltip:AddLine(" ")
+	DT.tooltip:AddDoubleLine("|CFFFFFFFFLeft click:|r", "toggle Details")
+	DT.tooltip:AddDoubleLine("|CFFFFFFFFRight click:|r", "open Settings")
 	DT.tooltip:Show()
 end
 
 local function OnEvent(self, event, ...)
-    local color = SpectraUI.Addons.Details and "|CFFFFFFFF" or "|CFFF63939"
-    self.text:SetText(color .. "Details|r")
+	local color = SpectraUI.Addons.Details and "|CFFFFFFFF" or "|CFFF63939"
+	self.text:SetText(color .. "Details|r")
 end
 
-local function OnClick()
-  if SpectraUI.Addons.Details then
-    SpectraUI:DetailsEmbeddedToggle()
-  end
+local function OnClick(_, button)
+	if button == "LeftButton" and SpectraUI.Addons.Details then
+		SpectraUI:DetailsEmbeddedToggle()
+	else
+		if (_G.DetailsOptionsWindow) then
+			if (_G.DetailsOptionsWindow:IsShown()) then
+				_G.DetailsOptionsWindow:Hide()
+				return
+			end
+		end
+
+		local lower_instance = Details:GetLowerInstanceNumber()
+		if (not lower_instance) then
+			local instance = Details:GetInstance(1)
+			Details.CriarInstancia (_, _, 1)
+			Details:OpenOptionsWindow (instance)
+		else
+			Details:OpenOptionsWindow (Details:GetInstance(lower_instance))
+		end
+	end
 end
 
 local function OnLeave(self)
