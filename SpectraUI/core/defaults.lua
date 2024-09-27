a2c019770129ea01e5a393ad0aab77d1e04fd5fc
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-09-27 16:32:28 +0200

    🧽 UPDATE: split defaults db

---
+++ SpectraUI/core/defaults.lua
@@ -0,0 +1,11 @@
+local E, _, V, P, G = unpack(ElvUI)
+-- dont touch this ^
+
+-- Create a unique table for our plugin
+-- Default settings DB for the addon
+P.SpectraUI = {
+	portraitsOffset = 5, -- Portrait zoom offest, default setting
+	detailsEmbedded = { -- settings for the embedded system
+		chatEmbedded = "RightChat",
+	},
+}
