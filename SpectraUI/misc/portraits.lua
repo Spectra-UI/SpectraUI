cd2eda78629a984d9cbd978a69d769745142f82a
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-01-03 19:17:49 +0100

    🧷 UPDATE: add thin diamond texture

--- SpectraUI/misc/portraits.lua
+++ SpectraUI/misc/portraits.lua
@@ -33,53 +33,79 @@ function SpectraUI:AddPortraitsTextures()
 			eliteborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\rare_border.tga",
 			eliteshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\rare_shadow.tga",
 
-			boss = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border.tga",
-			bossborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border_border.tga",
-			bossshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\border_shadow.tga",
+			boss = "Interface\\AddOns\\SpectraUI\\media\\portrait\\boss_texture.tga",
+			bossborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\boss_border.tga",
+			bossshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\boss_shadow.tga",
 		}
-	else
-		mMT.Media.CustomPortraits.spectraui.offset = E.db.SpectraUI.portraitsOffset
 	end
+	mMT.Media.CustomPortraits.spectraui.offset = E.db.SpectraUI.portraitsOffset
+
+	if not mMT.Media.CustomPortraits["spectraui_diamond"] then
+		mMT.Media.CustomPortraits["spectraui_diamond"] = {
+			name = SpectraUI.Name .. " Diamond", -- name to show
+
+			-- portrait texture part
+			texture = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_texture.tga",
+			border = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_border.tga",
+			shadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_shadow.tga",
+			inner = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_inner.tga",
+
+			-- else use this
+			extraMask = false,
+			mask = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_mask.tga",
+
+			-- extra textures for rare, elite and boss
+			rare = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_texture.tga",
+			rareborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_border.tga",
+			rareshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_shadow.tga",
+
+			elite = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_texture.tga",
+			eliteborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_border.tga",
+			eliteshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_rare_shadow.tga",
+
+			boss = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_boss_texture.tga",
+			bossborder = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_boss_border.tga",
+			bossshadow = "Interface\\AddOns\\SpectraUI\\media\\portrait\\diamond_boss__shadow.tga",
+		}
+	end
+	mMT.Media.CustomPortraits.spectraui_diamond.offset = E.db.SpectraUI.portraitsOffset
 end
 
 local elements = {}
 
 local function ShowHideElements(show)
-    for _, element in ipairs(elements) do
-        if element then
-            element:SetShown(show)
-        end
-    end
+	for _, element in ipairs(elements) do
+		if element then element:SetShown(show) end
+	end
 end
 
 function SpectraUI:PLAYER_TARGET_CHANGED(event, unit)
-    local targetExists = UnitExists("target")
-    ShowHideElements(not targetExists)
+	local targetExists = UnitExists("target")
+	ShowHideElements(not targetExists)
 end
 
 function SpectraUI:PlayerPortrait()
-    if mMT and mMT.Modules.Portraits and _G.mMT_Portrait_Player then
-        elements = {
-            _G.mMT_Portrait_Player.texture,
-            _G.mMT_Portrait_Player.portrait,
-            _G.mMT_Portrait_Player.iconbg,
-            _G.mMT_Portrait_Player.shadow,
-            _G.mMT_Portrait_Player.innerShadow,
-            _G.mMT_Portrait_Player.border,
-            _G.mMT_Portrait_Player.extraBorder,
-            _G.mMT_Portrait_Player.extraShadow,
-            _G.mMT_Portrait_Player.corner,
-            _G.mMT_Portrait_Player.cornerBorder,
-        }
+	if mMT and mMT.Modules.Portraits and _G.mMT_Portrait_Player then
+		elements = {
+			_G.mMT_Portrait_Player.texture,
+			_G.mMT_Portrait_Player.portrait,
+			_G.mMT_Portrait_Player.iconbg,
+			_G.mMT_Portrait_Player.shadow,
+			_G.mMT_Portrait_Player.innerShadow,
+			_G.mMT_Portrait_Player.border,
+			_G.mMT_Portrait_Player.extraBorder,
+			_G.mMT_Portrait_Player.extraShadow,
+			_G.mMT_Portrait_Player.corner,
+			_G.mMT_Portrait_Player.cornerBorder,
+		}
 
-        SpectraUI:PLAYER_TARGET_CHANGED()
+		SpectraUI:PLAYER_TARGET_CHANGED()
 
-        if E.db.SpectraUI.playerPortraitHide then
-            SpectraUI:RegisterEvent("PLAYER_TARGET_CHANGED")
-        else
-            SpectraUI:UnregisterEvent("PLAYER_TARGET_CHANGED")
-            ShowHideElements(true)
-        end
-    end
+		if E.db.SpectraUI.playerPortraitHide then
+			SpectraUI:RegisterEvent("PLAYER_TARGET_CHANGED")
+		else
+			SpectraUI:UnregisterEvent("PLAYER_TARGET_CHANGED")
+			ShowHideElements(true)
+		end
+	end
 end
-
