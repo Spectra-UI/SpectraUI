2c0d8f2e222610cceae77b84fe8c0d64b01a5852
SpectraUI <andershagman91@gmail.com>
2026-01-02 22:42:55 +0100

    new fonts
    added and removed some fonts for testing

--- ElvUI_SpectraUI/media/SharedMedia.lua
+++ ElvUI_SpectraUI/media/SharedMedia.lua
@@ -14,9 +14,10 @@ LSM:Register("border", "SpectraUI Nova Dark", [[Interface\AddOns\ElvUI_SpectraUI
 --LSM:Register("font", "SpectraUI Font 1", [[Interface\AddOns\ElvUI_SpectraUI\media\font\dummy.ttf]])
 LSM:Register("font", "SpectraUI Caption", [[Interface\AddOns\ElvUI_SpectraUI\media\font\GothamXNarrow-Black.ttf]])
 LSM:Register("font", "SpectraUI Title", [[Interface\AddOns\ElvUI_SpectraUI\media\font\GothamXNarrow-BlackCAPS.ttf]])
-LSM:Register("font", "SpectraUI Test 18", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_18pt-Bold.ttf]])
-LSM:Register("font", "SpectraUI Test 24", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_24pt-Bold.ttf]])
-LSM:Register("font", "SpectraUI Test 28", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_28pt-Bold.ttf]])
+LSM:Register("font", "SpectraUI Black", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_18pt-Black.ttf]])
+LSM:Register("font", "SpectraUI ExtraBold", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_24pt-ExtraBold.ttf]])
+LSM:Register("font", "SpectraUI Bold", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_28pt-Bold.ttf]])
+LSM:Register("font", "SpectraUI Regular", [[Interface\AddOns\ElvUI_SpectraUI\media\font\Inter_28pt-Regular.ttf]])
 
 -- SOUND
 --LSM:Register("sound", "SpectraUI Sound 1", [[Interface\AddOns\ElvUI_SpectraUI\media\sound\dummy.mp3]])
